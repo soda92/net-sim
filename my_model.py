d805f0a48f62d22cf0e0e4d1cbd02704f3a3b17a
SodaCris <wang-jiaxun@outlook.com>
2022-07-03 11:25:32 +0800

    add info spread

--- my_model.py
+++ my_model.py
@@ -1,5 +1,6 @@
 """Information models"""
 import enum
+import random
 import networkx as graph_tools
 
 
@@ -20,6 +21,7 @@ class MyGraphNode:  # pylint: disable=too-many-instance-attributes
         self.id_ = id_
         self.name = f"node#{id_}"
         self.node_state: Info = Info.INFO_S
+        self.node_state_next: Info = Info.INFO_S
         self.active: float = 0
 
         self.mass_frag: list[Info] = []
@@ -45,6 +47,16 @@ class MyGraphNode:  # pylint: disable=too-many-instance-attributes
 
     def __repr__(self):
         return self.name
+
+
+def info_list_to_str(info_list: list[Info]) -> str:
+    "convert list of info to string"
+    return ",".join(map(lambda x: x.name, info_list))
+
+
+def str_to_info_list(string: str) -> list[Info]:
+    "from string construct list of info"
+    return list(map(lambda x: Info[x], string.split(",")))
 
 
 class MyGraph:
@@ -75,7 +87,7 @@ class MyGraph:
             nx_node["source"] = node.name
             nx_node["node_state"] = node.node_state.name
             nx_node["active"] = node.active
-            nx_node["mass_frag"] = ','.join(node.mass_frag)
+            nx_node["mass_frag"] = info_list_to_str(node.mass_frag)
             nx_node["mass_count"] = node.mass_count
             nx_node["mass_rarity"] = node.mass_rarity
             nx_node["node_emotion_self"] = node.node_emotion_self
@@ -90,10 +102,29 @@ class MyGraph:
     # def from_nx_graph(self, nx_graph: graph_tools.Graph):
     #     self.nodes = [MyGraphNode(id_=i) for i in range(len(nx_graph.nodes))]
 
+    def clock(self):
+        "spread one time"
+        for node in self.nodes:
+            if node.node_state != Info.INFO_S:
+                for neighbor in node.neighbors:
+                    spread_rate = 0.5
+                    if random.random() > spread_rate:
+                        neighbor.node_state_next = node.node_state
+        for node in self.nodes:
+            if node.node_state_next != Info.INFO_S:
+                node.node_state = node.node_state_next
+                node.node_state_next = Info.INFO_S
+
+    def clockn(self, times: int):
+        "spread n times"
+        for _ in range(times):
+            self.clock()
+
 
 if __name__ == "__main__":
     mygraph = MyGraph()
-    mygraph.create_graph(num_nodes=10, minium_edges_per_node=3)
+    mygraph.create_graph(num_nodes=100, minium_edges_per_node=3)
+    mygraph.nodes[0].node_state = Info.INFO1
+    mygraph.clockn(3)
     converted = mygraph.to_nx_graph()
-    graph_tools.write_gexf(G=converted, path="converted.gexf")
     graph_tools.write_graphml(G=converted, path="converted.graphml")
